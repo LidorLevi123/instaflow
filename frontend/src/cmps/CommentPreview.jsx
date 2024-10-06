d5a1ccda451c26078948ad80d8070a2609ff89b4
LidorLevi123 <lidorlevi728@gmail.com>
2024-10-06 22:38:48 +0300

    add comment preview styling

--- frontend/src/cmps/CommentPreview.jsx
+++ frontend/src/cmps/CommentPreview.jsx
@@ -4,11 +4,11 @@ export function CommentPreview({ comment }) {
     return (
         <article className="comment-preview">
             <img src={comment.by.imgUrl} alt="" />
-            <div>
+            <div className="user">
                 <span className="fullname">{comment.by.fullname}</span>
                 <span className="txt">{comment.txt}</span>
             </div>
-            <div>
+            <div className="actions">
                 <span className="created-at">{getTimeSince(comment.createdAt)}</span>
                 <span className="likes">{comment.likedBy?.length} likes</span>
                 <span className="btn-reply">Reply</span>
