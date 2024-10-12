7f924beb57c30f4117551f643f2be858e6fb51f5
LidorLevi123 <lidorlevi728@gmail.com>
2024-10-12 18:17:00 +0300

    add a login page

--- frontend/src/RootCmp.jsx
+++ frontend/src/RootCmp.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { Routes, Route } from 'react-router'
 
 import { UserMsg } from './cmps/UserMsg.jsx'
-import { LoginSignup } from './pages/LoginSignup.jsx'
 import { FeedIndex } from './pages/FeedIndex.jsx'
 import { HomePage } from './pages/HomePage.jsx'
+import { LoginPage } from './pages/LoginPage.jsx'
 
 export function RootCmp() {
     return (
@@ -15,7 +15,7 @@ export function RootCmp() {
                     <Route path="" element={<FeedIndex />} >
                         <Route path="" element={<HomePage />} />
                     </Route>
-                    <Route path="login" element={<LoginSignup />} />
+                    <Route path="login" element={<LoginPage />} />
                 </Routes>
             </main>
         </div>
---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router'
+import { NavLink } from 'react-router-dom'
+
+import { login } from '../store/actions/user.actions'
+
+export function LoginPage() {
+    const [userCreds, setUserCreds] = useState({ username: '', password: '' })
+    const navigate = useNavigate()
+
+    async function onLogin(ev = null) {
+        if (ev) ev.preventDefault()
+
+        if (!userCreds.username) return
+        await login(userCreds)
+        navigate('/')
+    }
+
+    function handleChange(ev) {
+        const field = ev.target.name
+        const value = ev.target.value
+
+        setUserCreds(prevCreds => ({
+            ...prevCreds,
+            [field]: value
+        }))
+    }
+
+    return (
+        <section className="login-page">
+            <div>
+                <div className="logo">Instagram</div>
+
+                <form onSubmit={onLogin}>
+                    <input
+                        type="text"
+                        placeholder="Phone number, username, or email"
+                        name="username"
+                        onChange={handleChange}
+                        value={userCreds.username} />
+                    <input
+                        type="text"
+                        placeholder="Password"
+                        name="password"
+                        onChange={handleChange}
+                        value={userCreds.password} />
+                    <button className="btn-login">Log in</button>
+                </form>
+
+                <div>
+                    <hr /> OR <hr />
+                </div>
+
+                <p>Log in with Facebook</p>
+                <small>Continue as a Guest</small>
+            </div>
+
+            <div>
+                <p>Don't have an account? <NavLink to="signup">Sign up</NavLink></p>
+            </div>
+        </section>
+    )
+}
