83e5682bc7a02da278e519b2d11469e5ad89002a
LidorLevi123 <lidorlevi728@gmail.com>
2024-11-03 23:09:17 +0200

    add a route guard to unauthenticated users

--- frontend/src/RootCmp.jsx
+++ frontend/src/RootCmp.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route } from 'react-router'
+import { Routes, Route, Navigate } from 'react-router'
 
 import { UserMsg } from './cmps/UserMsg.jsx'
 import { FeedIndex } from './pages/FeedIndex.jsx'
@@ -7,6 +7,13 @@ import { HomePage } from './pages/HomePage.jsx'
 import { LoginPage } from './pages/LoginPage.jsx'
 import { SignupPage } from './pages/SignupPage.jsx'
 import { UserPage } from './pages/UserPage.jsx'
+import { userService } from './services/user/user.service.local.js'
+
+function RouteGuard({ children }) {
+    const user = userService.getLoggedinUser()
+    if (!user) return <Navigate to="/login" />
+    return children
+}
 
 export function RootCmp() {
     return (
@@ -14,7 +21,7 @@ export function RootCmp() {
             <UserMsg />
             <main>
                 <Routes>
-                    <Route path="" element={<FeedIndex />} >
+                    <Route path="" element={<RouteGuard><FeedIndex /></RouteGuard>} >
                         <Route path="" element={<HomePage />} />
                         <Route path="user/:userId" element={<UserPage />} />
                     </Route>
