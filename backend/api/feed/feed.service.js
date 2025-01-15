c4f8f38bbb745e1094e86ebfd1b17e2a5c82f204
Lidor Levi <134968138+LidorLevi123@users.noreply.github.com>
2025-01-15 09:03:30 +0200

    get a feed without aggregation

--- backend/api/feed/feed.service.js
+++ backend/api/feed/feed.service.js
@@ -38,40 +38,63 @@ async function query(criteria = {}) {
 async function getById(feedId) {
 	try {
 		const criteria = { _id: ObjectId.createFromHexString(feedId) }
-		const collection = await dbService.getCollection('feed')
+		const feedsCollection = await dbService.getCollection('feed')
+		const feed = await feedsCollection.findOne(criteria)
 
-		const results = await collection
-			.aggregate([
-				{
-					$match: criteria
-				},
-				{
-					$lookup: {
-						from: 'comment',
-						localField: '_id',
-						foreignField: 'aboutFeedId',
-						as: 'comments',
-					},
-				},
-			])
-			.toArray()
+		const commentObjIds = feed.commentIds.map(ObjectId.createFromHexString)
+		const commentsCollection = await dbService.getCollection('comment')
+		const commentsCursor = await commentsCollection.find({ _id: { $in: commentObjIds } })
 
-		const feed = {
-			...results[0],
-			comments: results[0].comments.map(comment => {
-				delete comment.aboutFeedId
-				return comment
-			})
-		}
+		feed.comments = commentsCursor.toArray().map(comment => {
+			delete comment.aboutFeedId
+			return comment
+		})
 
 		delete feed.commentIds
-
 		return feed
 	} catch (err) {
 		logger.error(`while finding feed ${feedId}`, err)
 		throw err
 	}
 }
+
+// async function getById(feedId) {
+// 	try {
+// 		const criteria = { _id: ObjectId.createFromHexString(feedId) }
+// 		const collection = await dbService.getCollection('feed')
+
+// 		const results = await collection
+// 			.aggregate([
+// 				{
+// 					$match: criteria
+// 				},
+// 				{
+// 					$lookup: {
+// 						from: 'comment',
+// 						localField: '_id',
+// 						foreignField: 'aboutFeedId',
+// 						as: 'comments',
+// 					},
+// 				},
+// 			])
+// 			.toArray()
+
+// 		const feed = {
+// 			...results[0],
+// 			comments: results[0].comments.map(comment => {
+// 				delete comment.aboutFeedId
+// 				return comment
+// 			})
+// 		}
+
+// 		delete feed.commentIds
+
+// 		return feed
+// 	} catch (err) {
+// 		logger.error(`while finding feed ${feedId}`, err)
+// 		throw err
+// 	}
+// }
 
 async function remove(feedId) {
 	// const { loggedinUser } = asyncLocalStorage.getStore()
@@ -112,8 +135,8 @@ async function update(feed) {
 		likedBy: feed.likedBy,
 		commentIds: feed.commentIds
 		// imgUrls: feed.imgUrls,
-		// comments: feed.comments,
 	}
+	
 	try {
 		const criteria = { _id: ObjectId.createFromHexString(feed._id) }
 
