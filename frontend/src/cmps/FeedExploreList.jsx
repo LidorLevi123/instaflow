88d9fc70c98d270b2dd93e4bef3c8947dd338d37
LidorLevi123 <lidorlevi728@gmail.com>
2025-01-19 23:57:21 +0200

    add hovering effect at feed explore list

--- frontend/src/cmps/FeedExploreList.jsx
+++ frontend/src/cmps/FeedExploreList.jsx
@@ -5,6 +5,13 @@ export function FeedExploreList({ feeds }) {
                 feeds.map(feed =>
                     <li key={feed._id}>
                         <img src={feed.imgUrls[0]} alt="Post preview" />
+                        <div className="info">
+                            <img src="/img/heart.png" alt="" />
+                            <span>{feed.likedBy?.length || 0}</span>
+                            
+                            <img src="/img/comment.png" alt="" />
+                            <span>{feed.comments?.length || 0}</span>
+                        </div>
                     </li>)
             }
         </ul>
