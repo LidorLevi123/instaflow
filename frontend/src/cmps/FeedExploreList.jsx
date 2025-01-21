d7eb85e6f9e4ba58a092117148de306c1d22e931
LidorLevi123 <lidorlevi728@gmail.com>
2025-01-21 21:51:34 +0200

    make feeds clickable at explore page

--- frontend/src/cmps/FeedExploreList.jsx
+++ frontend/src/cmps/FeedExploreList.jsx
@@ -1,14 +1,23 @@
+import { useSearchParams } from "react-router-dom"
+
 export function FeedExploreList({ feeds }) {
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    function onOpenFeedDetails(feedId) {
+        searchParams.set('feedId', feedId)
+        setSearchParams(searchParams)
+    }
+
     return (
         <ul className="feed-explore-list">
             {
                 feeds.map(feed =>
-                    <li key={feed._id}>
+                    <li key={feed._id} onClick={() => onOpenFeedDetails(feed._id)}>
                         <img src={feed.imgUrls[0]} alt="Post preview" />
                         <div className="info">
                             <img src="/img/heart.png" alt="" />
                             <span>{feed.likedBy?.length || 0}</span>
-                            
+
                             <img src="/img/comment.png" alt="" />
                             <span>{feed.comments?.length || 0}</span>
                         </div>
